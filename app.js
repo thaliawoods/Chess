f5391958c45ec405b705bedbcb17d3c1c4d4b865
thaliawoods <thaliadelano@gmail.com>
2024-02-27 15:10:30 +0100

    Moves

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const infoDisplay = document.querySelector("#info-display")
 const width = 8
 
 
+// gameboard
 const startPieces = [
     rook, knight, bishop, queen, king, bishop, knight, rook,
     pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn,
@@ -18,12 +19,13 @@ const startPieces = [
     rook, knight, bishop, queen, king, bishop, knight, rook
 ]
 
-
+// create squares on the Board and add pieces
 function createBoard() {
     startPieces.forEach((startPiece, i) => {
         const square = document.createElement('div')
         square.classList.add('square')
         square.innerHTML= startPiece
+        square.firstChild?.setAttribute('draggable', true)
         square.setAttribute('square-id', i)
         // square.classList.add('beige')
         const row = Math.floor((63 - i) / 8 ) + 1
@@ -32,10 +34,52 @@ function createBoard() {
         } else {
             square.classList.add(i % 2 === 0 ? "brown" : "beige")
         }
+
+        if ( i<= 15) {
+            square.firstChild.firstChild.classList.add('black')
+        }
+
+        if ( i>= 48) {
+            square.firstChild.firstChild.classList.add('white')
+        }
         gameBoard.append(square)
     })
 }
 
 createBoard()
 
-// 18:16
+
+
+
+
+// moves
+const allSquares = document.querySelectorAll("#gameboard .square")
+
+// console.log(allSquares)
+
+allSquares.forEach(square => {
+    square.addEventListener('dragstart', dragStart)
+    square.addEventListener('dragover', dragOver)
+    square.addEventListener('drop', dragDrop)
+})
+
+let startPositionId 
+let draggedElement
+
+function dragStart (e) {
+    startPositionId = e.target.parentNode.getAttribute('square-id')
+    draggedElement = e.target
+}
+
+function dragOver(e) {
+    e.preventDefault()
+    // console.log(e.target)
+}
+
+function dragDrop(e) {
+    e.stopPropagation()
+
+    // e.target.parentNode.append(draggedElement)
+    e.target.append(draggedElement)
+}
+
